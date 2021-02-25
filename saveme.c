25649dfbf20176ccb41da7ef91ef6cf8f6bc5272
Pranjal Srivastava <42092291+pranjals149@users.noreply.github.com>
2021-02-26 00:09:48 +0530

    Add files via upload

---
+++ saveme.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    printf("LEZDK5LCNVFHI");
+    for(int i=0;i<=2;i++)
+        printf("=");
+    printf("\nYou need to have a strong Base to decode this");
+    return 0;
+}
